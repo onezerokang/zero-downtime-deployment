f1659a2a66d3c22e818648747f8e4f810a86321e
onezerokang <onezerokang@gmail.com>
2023-07-12 20:12:37 +0900

    first commit

---
+++ src/test/java/com/onezerokang/zerodowntime/ZeroDowntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onezerokang.zerodowntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDowntimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
