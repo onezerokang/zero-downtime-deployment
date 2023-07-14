34a68efd569e2bba5f30765648b0231f61bd0c86
onezerokang <onezerokang@gmail.com>
2023-07-15 02:45:00 +0900

    update nohup script

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -16,7 +16,8 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground-logging/*.jar
+#nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground-logging/*.jar
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground-logging/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 echo "Now new WAS runs at ${TARGET_PORT}."
 
 exit 0
