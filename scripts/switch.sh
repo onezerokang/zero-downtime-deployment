9a3f748959c5ddef8278763b7ba5b40fb3220d83
onezerokang <onezerokang@gmail.com>
2023-07-15 01:59:26 +0900

    Update deploy script

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,27 +1,20 @@
-#!/bin/bash
-
-# Crawl current connected port of WAS
-CURRENT_PORT=$(cat /home/ubuntu/etc/nginx/conf.d/service_url.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
-echo "> Nginx currently proxies to ${CURRENT_PORT}."
+echo "Nginx currently proxies to ${CURRENT_PORT}."
 
-# Toggle port number
 if [ ${CURRENT_PORT} -eq 8081 ]; then
-    TARGET_PORT=8082
+  TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
-    TARGET_PORT=8081
+  TARGET_PORT=8081
 else
-    echo "> No WAS is connected to nginx"
-    exit 1
+  echo "No WAS is connected to nginx"
+  exit 1
 fi
 
-# Change proxying port into target port
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | sudo tee /home/ubuntu/etc/nginx/conf.d/service_url.inc
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-echo "> Now Nginx proxies to ${TARGET_PORT}."
+echo "Now Nginx proxies to ${TARGET_PORT}."
+sudo service nginx reload
 
-# Reload nginx
-sudo systemctl reload nginx
-
-echo "> Nginx reloaded."
+echo "Nginx reloaded."
